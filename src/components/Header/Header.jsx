a71debbe0afe4d0c7bbef8300cc8a514bc790aee
walaa-em <wal3a.em@gmail.com>
2023-04-05 16:56:37 +0200

    update build for gh-pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,16 +6,16 @@ export default class Header extends Component {
 
     return (
       <>
-            <header className='text-center py-2 vh-100'>
-                <img src="images/avataaars.svg" alt=""></img>
-                <h1>START REACT</h1>
-                <div className='py-3'>
-                    <span className='line'></span>
+        <header className='text-center py-2 vh-100'>
+          <img src="/reactAssign1/images/avataaars.svg" alt=""></img>
+          <h1>START REACT</h1>
+          <div className='py-3'>
+            <span className='line'></span>
             <i className="fa-solid fa-star fs-2"></i>
-                    <span className='line'></span>
-                </div>
+            <span className='line'></span>
+          </div>
 
-                <p>Graphic Artist - Web Designer - Illustrator</p>
+          <p>Graphic Artist - Web Designer - Illustrator</p>
 
         </header>
       </>
